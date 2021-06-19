7d81732e0a1122e7f32cdb10adc09a1edc6030de
Alper <alper.pazarlioglu@gmail.com>
2021-06-19 18:38:40 +0300

    Secret key was seperated from settings.py

--- MedSearch_Django_Project/MedSearch/MedSearch/settings.py
+++ MedSearch_Django_Project/MedSearch/MedSearch/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from secrets import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j-!3j0$7-a_ru_^wzu&11g691nz(zd@bt-+x-nv4ok+j^y$x%6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
