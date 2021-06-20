763d6453b8a0e97472bd690af7f2b30259659c08
Alper <alper.pazarlioglu@gmail.com>
2021-06-21 02:29:08 +0300

    added initial test cases

--- MedSearch_Django_Project/MedSearch/articles/tests.py
+++ MedSearch_Django_Project/MedSearch/articles/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import *
+from django.contrib.auth import get_user_model
+from .models import *
 
 # Create your tests here.
+
+class urlTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def Results(self):
+        response = self.client.get('results/')
+        self.assertEqual(response.status_code, 200)
+
+
+class modelTests(TestCase):
+    def test_articlemodel(self):
+        article = Article.objects.create(PMID="34130097",ArticleTitle="MOAT",ArticleDate="2021-06-30",AuthorList="James Joyce",AbstractText="synesthesia",ArticleKeywords="N/A")
+
+        self.assertEqual(article.PMID, "34130097")
+        self.assertEqual(article.ArticleTitle, "MOAT")
+        self.assertEqual(article.ArticleDate, "2021-06-30")
+        self.assertEqual(article.AuthorList, "James Joyce")
+        self.assertEqual(article.AbstractText, "synesthesia")
+        self.assertEqual(article.ArticleKeywords, "N/A")
+
+
+User = get_user_model()
+
+class userTests(TestCase):
+
+    def setUpUser(self):
+        user1=User(username='alper', email='alper001@gmail.com', )
+        user1.set_password('donkeykong1')
+        user1.save()
+
+    def testLogin(self):
+        user1= {"username": "alper", "password": "donkeykong1"}
+        response=self.client.post('login/', user1, follow=True)
--- MedSearch_Django_Project/MedSearch/articles/urls.py
+++ MedSearch_Django_Project/MedSearch/articles/urls.py
@@ -5,11 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.registerPage, name="Home"),
 
-    path('detail', views.retrieve_article_data, name="article"),
-    path('results', views.Results, name="Results"),
-    path('register', views.registerPage, name="Register"),
-    path('login', views.loginPage, name="Login"),
-    path('logout', views.logoutPage, name="Logout"),
-
+    path('detail/<int:pk>', views.retrieve_article_data, name="article"),
+    path('results/', views.Results, name="Results"),
+    path('register/', views.registerPage, name="Register"),
+    path('login/', views.loginPage, name="Login"),
+    path('logout/', views.logoutPage, name="Logout"),
 
 ]
