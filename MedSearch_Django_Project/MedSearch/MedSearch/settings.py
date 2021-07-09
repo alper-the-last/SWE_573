163c831049e2b690b89ea3444939496bced09dd6
Alper Pazarlioglu <81327082+alper-the-last@users.noreply.github.com>
2021-07-09 11:00:53 +0300

    Update settings.py

--- MedSearch_Django_Project/MedSearch/MedSearch/settings.py
+++ MedSearch_Django_Project/MedSearch/MedSearch/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'MedSearch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgresDB',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
